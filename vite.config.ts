96c3b8ffbb542468b8100407b7ae5e5c48bfefc8
Mircea Casapu <mircea.casapu@gmail.com>
2024-10-26 21:22:43 +0300

    chore: add components path alias

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: {
       "@@assets": path.resolve(__dirname, "./src/assets"),
       "@@pages": path.resolve(__dirname, "./src/pages"),
+      "@@components": path.resolve(__dirname, "./src/components"),
     },
   },
   server: {
