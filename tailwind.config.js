f59bd2ccd68e5abd6e6d876e7f46a3a2b41850e0
Mircea Casapu <mircea.casapu@gmail.com>
2024-10-26 14:03:01 +0300

    chore: add rbr specific colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rbrBlue: "#001a30",
+        rbrRed: "#ea1d2d",
+        rbrGrey: "#a8a9ac",
+        rbrYellow: "#fcd700",
+      },
+    },
   },
   plugins: [],
 };
