5b5604219f6663b2b86ee0d6b88cc1fbd013ebd7
Mircea Casapu <mircea.casapu@gmail.com>
2024-10-27 16:02:26 +0200

    chore: make rbr logo clickable in header

--- src/pages/root/components/Header.tsx
+++ src/pages/root/components/Header.tsx
@@ -1,11 +1,14 @@
 import RbrLogo from "@@assets/rbr_2024_logo.webp";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <header className="bg-rbrBlue">
-      <picture className="flex aspect-auto items-center p-6">
-        <img src={RbrLogo} alt="RBR Logo" className="w-24" />
-      </picture>
+      <Link to="/">
+        <picture className="flex aspect-auto items-center p-6">
+          <img src={RbrLogo} alt="RBR Logo" className="w-24" />
+        </picture>
+      </Link>
     </header>
   );
 };
