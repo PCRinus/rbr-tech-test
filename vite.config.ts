c11bec9f9d24cc34b8bde8a6c0602d081d49b6c0
Mircea Casapu <mircea.casapu@gmail.com>
2024-10-27 13:19:42 +0200

    add cn utility function for merging tw classes

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       "@@assets": path.resolve(__dirname, "./src/assets"),
       "@@pages": path.resolve(__dirname, "./src/pages"),
       "@@components": path.resolve(__dirname, "./src/components"),
+      "@@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
   server: {
